6cbf316115aab288ad68a92693a651fbf00d4ae7
carolinafmacedo <carolinamsn2010@gmail.com>
2026-05-22 20:41:20 -0300

    Prática 2 - Parte 1

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,29 +1,17 @@
 package com.example.weatherapp
 
-import android.R.attr.name
-import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.HomePage
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,29 +46,3 @@ fun GreetingPreview() {
     }
 }
 
-@Composable
-fun HomePage(modifier: Modifier = Modifier){
-    val activity = LocalContext.current as Activity
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = CenterHorizontally
-
-    ) {
-        val modifier = modifier.fillMaxWidth(fraction = 0.9f)
-        Text(
-            text = "Bem-vindo ao Weather App",
-            fontSize = 24.sp
-        )
-
-        Spacer(modifier = modifier.size(12.dp))
-
-        Button(  onClick = {
-            activity.finish()
-        }) {
-            Text("Sair")
-        }
-    }
-}
