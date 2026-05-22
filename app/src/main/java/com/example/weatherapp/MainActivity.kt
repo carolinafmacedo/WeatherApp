f66b307f33076658d7b9e20bf50daa6879ddf782
carolinafmacedo <carolinamsn2010@gmail.com>
2026-05-22 19:59:23 -0300

    Prática 1 – Parte 3

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.weatherapp
 
+import android.R.attr.name
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomePage(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -43,5 +55,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     WeatherAppTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier){
+    val activity = LocalContext.current as Activity
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally
+
+    ) {
+        val modifier = modifier.fillMaxWidth(fraction = 0.9f)
+        Text(
+            text = "Bem-vindo ao Weather App",
+            fontSize = 24.sp
+        )
+
+        Spacer(modifier = modifier.size(12.dp))
+
+        Button(  onClick = {
+            activity.finish()
+        }) {
+            Text("Sair")
+        }
     }
 }
